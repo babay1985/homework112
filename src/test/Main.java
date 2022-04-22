35167744efa1930224b8ec17ae50132048806cff
Aziz home work1 <ruhsari_0303@mail.ru>
2022-04-22 18:30:00 +0300

    Homework1

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Первый раз пишу");
+        System.out.println("Hello world");
     }
 }
